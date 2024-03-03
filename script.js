78bd308c6b9b47c5d952f1933968377e55adb47a
PrincessCS <princeschidera@gmail.com>
2024-03-03 21:38:31 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ class Telephone{
         if (this.phoneNumber.includes(number)) {
             console.log(`Dialing ${number}...`);
         } else {
-            console.log(`Could not dial ${number}. Number not found in the phone book.`);
+            console.log(`Could not dial ${number}. Number not found!`);
         }
     }
 
@@ -47,29 +47,55 @@ class Telephone{
     }
 }
 
+
 class updateObserver {
+
+    constructor(name){
+        this.name = name;
+    }
     update(action, number) {
-        console.log(`Phone number ${number} has been ${action}.`);
+        console.log(`${this.name}: Phone number ${number} has been ${action}.`);
     }
 }
 
+
+// creating an instance of the Telephone class
 const telephone = new Telephone();
-telephone.addPhoneNumber("12345679");
-telephone.addPhoneNumber("000000");
+
+//adding phonenumbers to the phoneNumber array
 telephone.addPhoneNumber("1234");
-telephone.removePhoneNumber("1234");
-telephone.removePhoneNumber("11111");
+telephone.addPhoneNumber("12345");
+telephone.addPhoneNumber("123456");
+telephone.addPhoneNumber("1234567");
 
-const observer = new updateObserver();
-observer.update("added", "1234");
-telephone.addObserver(observer);
+//removing a phonenumber
+telephone.removePhoneNumber("1234567");
+
+//dialling a removed phonenumber
+telephone.dialPhoneNumber("1234567");
 
 
 
-//observer.update("added", "12345679");
+//creating an instance of the updateObserver class
+const observer = new updateObserver("Observer 1");
+telephone.observers.push(observer);
+observer.update ("added", "12345");
+
 
 
 
 console.log(telephone);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
